4156cd4d335b8335a0c8adf763534f62204f18d3
ayamadori <ayamadori@gmail.com>
2020-03-09 00:41:41 +0900

    Initial commit

--- GSMTCStest/MainPage.xaml.cs
+++ GSMTCStest/MainPage.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Diagnostics;
+using Windows.Media.Control;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
@@ -22,9 +14,40 @@ namespace GSMTCStest
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // https://docs.microsoft.com/en-us/uwp/api/windows.media.control
+        // Need <uap7:Capability Name = "globalMediaControl" /> in appxmanifest
+        GlobalSystemMediaTransportControlsSessionManager manager;
+        GlobalSystemMediaTransportControlsSession session;
+        GlobalSystemMediaTransportControlsSessionMediaProperties properties;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            manager = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+            session = manager.GetCurrentSession();
+            session.MediaPropertiesChanged += Session_MediaPropertiesChanged;
+            session.PlaybackInfoChanged += Session_PlaybackInfoChanged;
+        }
+
+        private async void Session_PlaybackInfoChanged(GlobalSystemMediaTransportControlsSession sender, PlaybackInfoChangedEventArgs args)
+        {
+            var info = session.GetPlaybackInfo();
+            string status = "[PlaybackInfoChanged] PlaybackType: " + info.PlaybackType + ", Status: " + info.PlaybackStatus;
+            Debug.WriteLine(status);
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => { Status.Text = status; });
+        }
+
+        private async void Session_MediaPropertiesChanged(GlobalSystemMediaTransportControlsSession sender, MediaPropertiesChangedEventArgs args)
+        {
+            properties = await session.TryGetMediaPropertiesAsync();
+            string status = "[MediaPropertiesChanged] Artist: " + properties.Artist + ", Title: " + properties.Title;
+            Debug.WriteLine(status);
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => { Status.Text = status; });
         }
     }
 }
